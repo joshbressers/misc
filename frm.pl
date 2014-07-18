776fe17df58d491152717b17d612c8b1447a7395
bress <josh@bress.net>
2014-07-18 07:34:45 -0500

    Add the ability to catch various IRC commands in the middle of a line

--- frm.pl
+++ frm.pl
@@ -14,11 +14,25 @@ $VERSION = "2006092408";
 
 use Irssi 20020324;
 
+# These commands will be caught if they appear as is at the beginning of
+# the line.
 my @commands = ("frm", "ls", "s", "minc");
+
+# These commands will be caught if they appear anywhere other than the
+# beginning of the line
+my @illegal_terms = ("/wg", "/query", "/msg");
 
 sub cmd_remove {
     my ($msg, $server, $witem) = @_;
     my ($command);
+
+    foreach $command (@illegal_terms) {
+        if ($msg =~ /$command/ && $msg !~ /^$command/) {
+            return unless ($witem);
+            $witem->print("DON'T DO THAT!", MSGLEVEL_CLIENTCRAP);
+            Irssi::signal_stop();
+        }
+    }
 
     foreach $command (@commands) {
         if ($msg =~ /^$command$/) {
